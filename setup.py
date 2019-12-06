60e94cb52e6c22f06922959528bb0129c6d53eee
ChevalierDeBalibari <chevalierdebalibari@keemail.me>
2019-12-06 11:47:43 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='pythingy',
       version='0.1',
       description='Nordic Thingy52 Interface Module',
-      url='https://github.com/chevalierdebalibari/pythingy'
+      url='https://github.com/chevalierdebalibari/pythingy',
       author='Nebojsa Stojiljkovic',
       author_email='nebojsa@keemail.me',
-      license='MIT'
-      packages=['pythingy'],
+      license='MIT',
+      packages=['pythingy'], 
       zip_safe=False)
